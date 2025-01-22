639e6fc346487e2955568b5a2e79305e48b4c868
Serquand <esteban.vincent@efrei.net>
2025-01-22 10:56:24 +0100

    Optimize autorun.sh

--- java/autorun.sh
+++ java/autorun.sh
@@ -1,60 +1,15 @@
+#!/bin/bash
 mvn compile
-rm ./file.txt
+rm -f ./file.txt
 
-echo "" >> file.txt
-echo "Test for 10 elements and batch of 1000" >> file.txt
-rm contacts.sqlite3 && mvn -q exec:java -D exec.args="10 1000" >> file.txt
+declare -a elements=("10" "100" "1000" "10000" "50000" "100000" "1000000")
+declare -a batches=("1000" "500")
 
-echo "" >> file.txt
-echo "Test for 100 elements and batch of 1000" >> file.txt
-rm contacts.sqlite3 && mvn -q exec:java -D exec.args="100 1000" >> file.txt
-
-echo "" >> file.txt
-echo "Test for 1000 elements and batch of 1000" >> file.txt
-rm contacts.sqlite3 && mvn -q exec:java -D exec.args="1000 1000" >> file.txt
-
-echo "" >> file.txt
-echo "Test for 10000 elements and batch of 1000" >> file.txt
-rm contacts.sqlite3 && mvn -q exec:java -D exec.args="10000 1000" >> file.txt
-
-echo "" >> file.txt
-echo "Test for 50_000 elements and batch of 1000" >> file.txt
-rm contacts.sqlite3 && mvn -q exec:java -D exec.args="50000 1000" >> file.txt
-
-echo "" >> file.txt
-echo "Test for 100_000 elements and batch of 1000" >> file.txt
-rm contacts.sqlite3 && mvn -q exec:java -D exec.args="100000 1000" >> file.txt
-
-echo "" >> file.txt
-echo "Test for 1_000_000 elements and batch of 1000" >> file.txt
-rm contacts.sqlite3 && mvn -q exec:java -D exec.args="1000000 1000" >> file.txt
-
-################################################################
-
-echo "" >> file.txt
-echo "Test for 10 elements and batch of 500" >> file.txt
-rm contacts.sqlite3 && mvn -q exec:java -D exec.args="10 500" >> file.txt
-
-echo "" >> file.txt
-echo "Test for 100 elements and batch of 500" >> file.txt
-rm contacts.sqlite3 && mvn -q exec:java -D exec.args="100 500" >> file.txt
-
-echo "" >> file.txt
-echo "Test for 1000 elements and batch of 500" >> file.txt
-rm contacts.sqlite3 && mvn -q exec:java -D exec.args="1000 500" >> file.txt
-
-echo "" >> file.txt
-echo "Test for 10000 elements and batch of 500" >> file.txt
-rm contacts.sqlite3 && mvn -q exec:java -D exec.args="10000 500" >> file.txt
-
-echo "" >> file.txt
-echo "Test for 50_000 elements and batch of 500" >> file.txt
-rm contacts.sqlite3 && mvn -q exec:java -D exec.args="50000 500" >> file.txt
-
-echo "" >> file.txt
-echo "Test for 100_000 elements and batch of 500" >> file.txt
-rm contacts.sqlite3 && mvn -q exec:java -D exec.args="100000 500" >> file.txt
-
-echo "" >> file.txt
-echo "Test for 1_000_000 elements and batch of 500" >> file.txt
-rm contacts.sqlite3 && mvn -q exec:java -D exec.args="1000000 500" >> file.txt
+for batch in "${batches[@]}"; do
+    for element in "${elements[@]}"; do
+        echo "" >> file.txt
+        echo "Test for ${element} elements and batch of ${batch}" >> file.txt
+        rm -f contacts.sqlite3
+        mvn -q exec:java -D exec.args="${element} ${batch}" >> file.txt
+    done
+done
